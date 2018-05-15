cdac395a5c7e62c381e3dda5e9f26fbc298df27b
Shinkay <shinkayb@gmail.com>
2018-05-15 13:43:58 -0400

    remaining work

---
+++ app/data/friends.js
@@ -0,0 +1,102 @@
+var friends = [
+    {
+
+        "name": "Bradley Cooper",
+        "photo": "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Bradley_Cooper_%283699322472%29_%28cropped%29.jpg/220px-Bradley_Cooper_%283699322472%29_%28cropped%29.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Brad Pitt",
+        "photo": "http://sim02.in.com/25/fcda3f488ab424b52db23d7655d74e28_m.jpg",
+        "scores": [
+            5,
+            5,
+            5,
+            2,
+            3,
+            4,
+            2,
+            2,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Chris Hemsworth",
+        "photo": "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTU0ODUwMjQ0NjIwNzI0MDAx/chris-hemsworth-poses-during-a-photo-call-for-thor-ragnarok-on-october-15-2017-in-sydney-australia-photo-by-mark-metcalfe_getty-images-for-disney-square.jpg",
+        "scores": [
+            3,
+            1,
+            3,
+            4,
+            3,
+            1,
+            3,
+            5,
+            3,
+            1
+        ]
+    },
+    {
+        "name": "George Clooney",
+        "photo": "https://www.hellomagazine.com/imagenes/celebrities/2017053039337/George-Clooney-Amal-never-forgive-missed-birth/0-208-421/George%20Clooney-t.jpg",
+        "scores": [
+            1,
+            1,
+            4,
+            4,
+            5,
+            5,
+            4,
+            1,
+            3,
+            1
+        ]
+    },
+    {
+        "name": "Tom Brady",
+        "photo": "http://www.organicauthority.com/wp-content/uploads/2017/04/Tom-Brady-headshot-e1492111964348.png",
+        "scores": [
+            2,
+            1,
+            4,
+            4,
+            5,
+            3,
+            3,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Chris Evans",
+        "photo": "https://akns-images.eonline.com/eol_images/Entire_Site/2018019/rs_600x600-180119063216-600.chris-evans.11918.jpg?fit=around|700:700&crop=700:700;center,top&output-quality=90",
+        "scores": [
+            5,
+            3,
+            4,
+            4,
+            5,
+            4,
+            4,
+            1,
+            4,
+            1
+        ]
+    }
+
+];
+
+module.exports = friends;
--- server.js
+++ server.js
@@ -4,15 +4,16 @@ var bodyParser = require('body-parser');
 
 var PORT = process.env.PORT || 8080;
 
-var jsonParser = bodyParser.json()
-
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json({ type: 'application/*+json' }))
 
 app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
 
 app.use(bodyParser.text({ type: 'text/html' }))
+
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
 
 app.listen(PORT, function(){
     console.log("App listening on PORT: " + PORT);
